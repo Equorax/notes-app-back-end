44f02ffe4e4d0adb4fbe57ad5e31afa741e1c857
Equorax <pascalsanjaya34@gmail.com>
2026-05-29 20:23:52 +0700

    Add users & some auth

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,7 @@ import ErrorHandler from '../middlewares/error.js';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // ini adalah parser ubah JSON strings
 app.use(routes);
 app.use(ErrorHandler);
 
